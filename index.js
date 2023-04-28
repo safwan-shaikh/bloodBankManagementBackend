492a7bc5a73410f853495d7530ab7bcee88d5142
2201-safwan <117520007+safwan-shaikh@users.noreply.github.com>
2023-04-28 17:44:19 +0000

    routes setup in index

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors');
 
 const app = express();
-
+const bloodBankRoutes = require("./Routes/bankRoutes");
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(cors());
+
+app.use('/api/bloodBank', bloodBankRoutes);
 
 
 
